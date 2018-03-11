0923dc57590d2f846ee469cb038de74ea43158a7
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-11 18:01:16 -0400

    add new landing page image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+
   protect_from_forgery
 
   # before_action :disable_nav, only: [:landing_page]
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :users
     root to: "users#index"
   end
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
